5af031d5f9940abfcbbb0013d544105528e2b385
BuildTools <unconfigured@null.spigotmc.org>
2019-04-15 16:50:53 +0200

    Border Update 6.1

--- src/main/java/eu/mcone/citybuild/Inventorys/BorderInventory.java
+++ src/main/java/eu/mcone/citybuild/Inventorys/BorderInventory.java
@@ -17,7 +17,7 @@ public class BorderInventory extends CoreInventory {
         super("Bearbeite dein Rand", p, InventorySlot.ROW_3);
 
         setItem(InventorySlot.ROW_1_SLOT_1, new ItemBuilder(Material.DIAMOND_BLOCK, 1).displayName("§eDiamanten Rand").lore("§7Für §31000 Coins §7verfügbar!").create(), e -> setRand(plot,p,1000, "57"));
-        setItem(InventorySlot.ROW_1_SLOT_2, new ItemBuilder(Material.IRON_FENCE, 1).displayName("§eEisen-Gitter Rand").lore("§7Für §e1000 Coins §7verfügbar!").create(), e -> setRand(plot,p,1000, "101"));
+        setItem(InventorySlot.ROW_1_SLOT_2, new ItemBuilder(Material.IRON_FENCE, 1).displayName("§eEisen-Gitter Rand").lore("§7Für §31000 Coins §7verfügbar!").create(), e -> setRand(plot,p,1000, "101"));
         setItem(InventorySlot.ROW_1_SLOT_3, new ItemBuilder(Material.GLOWSTONE, 1).displayName("§eGlowstone Rand").lore("§7Für §31000 Coins §7verfügbar!").create(), e -> setRand(plot,p,1000, "89"));
         setItem(InventorySlot.ROW_1_SLOT_4, new ItemBuilder(Material.BEACON, 1).displayName("§eBeacon Rand").lore("§7Für §35000 Coins §7verfügbar!").create(), e -> setRand(plot,p,5000, "138"));
         setItem(InventorySlot.ROW_1_SLOT_5, new ItemBuilder(Material.SEA_LANTERN, 1).displayName("§eSee-Laternen Rand").lore("§7Für §35000 Coins §7verfügbar!").create(), e -> setRand(plot,p,5000, "169"));
