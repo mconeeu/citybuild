4cb1c57c5033c2cd9264044bfea199a36d9234d3
BuildTools <unconfigured@null.spigotmc.org>
2019-04-15 22:41:07 +0200

    - Sidebar fix - Boder fix - new Borders added - Pay fix - Deluxe added - Trader fix

--- src/main/java/eu/mcone/citybuild/listener/PlayerQuitListener.java
+++ src/main/java/eu/mcone/citybuild/listener/PlayerQuitListener.java
@@ -1,7 +1,9 @@
 package eu.mcone.citybuild.listener;
 
+import eu.mcone.citybuild.Citybuild;
 import eu.mcone.coresystem.api.bukkit.CoreSystem;
 import eu.mcone.coresystem.api.bukkit.player.CorePlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAchievementAwardedEvent;
@@ -12,11 +14,13 @@ public class PlayerQuitListener implements Listener {
 
     @EventHandler
     public void on(PlayerQuitEvent e) {
-        for (CorePlayer t : CoreSystem.getInstance().getOnlineCorePlayers()) {
-            if (!t.getUuid().equals(e.getPlayer().getUniqueId())) {
-                t.getScoreboard().getObjective(DisplaySlot.SIDEBAR).reload();
+        Bukkit.getScheduler().runTask(Citybuild.getInstance(), () -> {
+            for (CorePlayer t : CoreSystem.getInstance().getOnlineCorePlayers()) {
+                if (!t.getUuid().equals(e.getPlayer().getUniqueId())) {
+                    t.getScoreboard().getObjective(DisplaySlot.SIDEBAR).reload();
+                }
             }
-        }
+        });
     }
 
 }
