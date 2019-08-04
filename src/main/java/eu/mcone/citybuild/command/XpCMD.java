2647fdbd907f1bcbdaba0891129cff2769b1a791
Marvin Hülsmann <mrntv64@gmail.com>
2019-08-05 00:17:16 +0200

    Xp Bug fixed

--- src/main/java/eu/mcone/citybuild/command/XpCMD.java
+++ src/main/java/eu/mcone/citybuild/command/XpCMD.java
@@ -21,13 +21,13 @@ public class XpCMD extends CoreCommand {
         if (args.length == 0 && sender instanceof Player) {
             Player p = (Player) sender;
 
-            p.sendMessage("§7Du hast momentan §d" + p.getLevel() + " Level§7!");
+            p.sendMessage("§8[§7§l!§8] §dLevel §8» §7Du hast momentan §d" + p.getLevel() + " Level§7!");
 
             p.playSound(p.getLocation(), Sound.ENTITY_CHICKEN_EGG, 1, 1);
         } else if (sender.hasPermission("citybuild.xp")) {
             if (args.length == 1) {
                 if (args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("remove")) {
-                    CoreSystem.getInstance().getMessager().send(sender, "§4Bitte benutze: §c/xp <add | remove> <name> <amount> §4oder §c/xp <name>");
+                    CoreSystem.getInstance().getMessager().send(sender, "§4Bitte benutze: §c/xp oder /level <add | remove> <name> <amount> §4oder §c/xp oder /level <name>");
                     return true;
                 } else {
                     Player t = Bukkit.getPlayer(args[0]);
@@ -45,7 +45,7 @@ public class XpCMD extends CoreCommand {
 
                     if (t != null) {
                         t.setLevel(Integer.valueOf(args[2]));
-                        sender.sendMessage("§8[§7§l!§8] §dLevel §8» §7Du hast §f" + t.getName() + " §7erfolgreich §f" + args[2] + " §7Kisten hinzugefügt");
+                        sender.sendMessage("§8[§7§l!§8] §dLevel §8» §7Du hast §f" + t.getName() + " §7erfolgreich §d" + args[2] + " Level§7 hinzugefügt");
                     } else {
                         sender.sendMessage("§8[§7§l!§8] §dLevel §8» §cDer Spieler ist nicht online!");
                     }
@@ -70,7 +70,7 @@ public class XpCMD extends CoreCommand {
                 }
             }
 
-            CoreSystem.getInstance().getMessager().send(sender, "§4Bitte benutze: §c/Level <add | remove> <name> <amount> §4oder §c/Level <name>");
+            CoreSystem.getInstance().getMessager().send(sender, "4Bitte benutze: §c/xp oder /level <add | remove> <name> <amount> §4oder §c/xp oder /level <name>");
         } else if (sender instanceof Player) {
             CoreSystem.getInstance().getMessager().sendTransl((Player) sender, "system.command.noperm");
         }
